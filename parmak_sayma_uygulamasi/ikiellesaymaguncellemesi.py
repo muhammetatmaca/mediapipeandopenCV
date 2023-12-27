68d0d1d5c7f4c5cf438f20e919dbb4352113e445
muhammetatmaca <113444449+muhammetatmaca@users.noreply.github.com>
2023-12-28 01:21:42 +0300

    Update ikiellesaymaguncellemesi.py

--- parmak_sayma_uygulamasi/ikiellesaymaguncellemesi.py
+++ parmak_sayma_uygulamasi/ikiellesaymaguncellemesi.py
@@ -1,6 +1,5 @@
 import cv2
 import mediapipe as mp
-import time
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
@@ -8,46 +7,58 @@ cap.set(4, 480)
 
 mpHand = mp.solutions.hands
 hands = mpHand.Hands()
-mpDraw = mp.solutions.drawing_utils
+mpDraw = mp.solutions.drawing_utils 
 
 tipIds = [4, 8, 12, 16, 20]
-cTime = 0
-pTime = 0
-
 while True:
+    
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
+    
     results = hands.process(imgRGB)
-
+    # print(results.multi_hand_landmarks)
+    
+    lmList = []
     if results.multi_hand_landmarks:
-        finger_counts = []
-        for hand_num, handLms in enumerate(results.multi_hand_landmarks):
+        for handLms in results.multi_hand_landmarks:
             mpDraw.draw_landmarks(img, handLms, mpHand.HAND_CONNECTIONS)
-
-            lmList = []  # lmList değişkenini burada doldurun
+    
             for id, lm in enumerate(handLms.landmark):
                 h, w, _ = img.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
+                cx, cy = int(lm.x*w), int(lm.y*h)
                 lmList.append([id, cx, cy])
-
-            fingers = []  # fingers değişkenini burada tanımlayın
-
-            # Baş parmak
-            if lmList[tipIds[0]][2] < lmList[tipIds[0] - 1][2]:
+                
+                # # isaret uc = 8
+                # if id == 8:
+                #     cv2.circle(img, (cx,cy), 9, (255,0,0), cv2.FILLED)
+                # # isaret uc = 6
+                # if id == 6:
+                #     cv2.circle(img, (cx,cy), 9, (0,0,255), cv2.FILLED)
+       
+    if len(lmList) != 0:
+        fingers = []
+               
+        # bas parmak
+        if lmList[tipIds[0]][1] < lmList[tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+        
+        # 4 parmak
+        for id in range(1, 5):
+            
+            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
                 fingers.append(1)
             else:
                 fingers.append(0)
-
-            # Diğer 4 parmak
-            for id in range(1, 5):
-                if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
-                    fingers.append(1)
-                else:
-                    fingers.append(0)
-
-            totalF = fingers.count(1)
-            finger_counts.append(totalF)
-
+            
+        # print(fingers)
+    
+        totalF = fingers.count(1)
+        # print(totalF)
+        cv2.putText(img, str(totalF), (30,125), cv2.FONT_HERSHEY_PLAIN, 10, (255,0,0),8)
         
+    cv2.imshow("img",img)
+    cv2.waitKey(1)
+    
 
